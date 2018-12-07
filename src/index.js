bd4c2cf9ece3df318ddab2b54698d896aa98201e
Tim Toohey <ttoohey@users.noreply.github.com>
2018-12-07 22:07:43 +1100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,145 @@
+export class ValidatorError extends Error {
+  constructor(results, ...params) {
+    super(...params);
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, ValidatorError);
+    }
+    this.validatorErrorPayload = results;
+  }
+}
+
+/**
+ * Creates a validator function based on the provided rules
+ * @param  {object} rules - Keys are the field names; the values are a list of
+ *     rule functions to apply to that field
+ * @return {[object]} - A list of failing rule objects containing a
+ *     { type, prop } pair that identifies the rule that failed and the field
+ *     that triggered it
+ */
+export function createValidator(rules) {
+  const validate = validator(rules);
+  const state = {
+    change: {},
+    timeline: 0,
+    active: false,
+    blocked: []
+  };
+  return async change => {
+    state.change = { ...state.change, ...change };
+    const timeline = ++state.timeline;
+    if (state.active) {
+      await new Promise(resolve => state.blocked.push(resolve));
+      if (timeline !== state.timeline) {
+        throw new ValidatorError(null);
+      }
+    }
+    state.active = true;
+    const result = await validate(state.change);
+    state.active = false;
+    state.blocked.forEach(resolve => resolve());
+    state.blocked = [];
+    if (timeline !== state.timeline) {
+      throw new ValidatorError(result);
+    }
+    state.change = {};
+    state.timeline = 0;
+    return result;
+  };
+}
+
+function validator(rules) {
+  return async change => {
+    const asyncRules = [];
+    const asyncResult = {};
+    const getAsyncResult = async (rule, prop) =>
+      (await asyncResult[rule]).reduce(
+        (result, item) => result || (item && item.prop === prop ? item : null),
+        null
+      );
+    return (await Promise.all(
+      Object.keys(change)
+        .filter(prop => rules.hasOwnProperty(prop))
+        .map(async prop => {
+          for (let rule of rules[prop]) {
+            let result;
+            if (!asyncRules.includes(rule)) {
+              result = rule(change, prop);
+            } else {
+              result = await getAsyncResult(rule, prop);
+            }
+            if (Promise.resolve(result) === result) {
+              asyncRules.push(rule);
+              asyncResult[rule] = result;
+              result = await getAsyncResult(rule, prop);
+            }
+            if (result === null) continue;
+            return result;
+          }
+          return null;
+        })
+    )).filter(result => result !== null);
+  };
+}
+
+/**
+ * Helper for defining simple validation rules
+ * @param  {string} type - names the rule
+ * @param  {function} validate - callback that accepts a value and returns a
+ *     boolean; does the value pass or fail the rule
+ * @param  {function|*} payload - data to include in the result if the rule
+ *     fails. If a function is provided it will be called with the result object
+ *     and the response used as result.payload
+ * @return {function} - function to call to apply the rule to a change; typically
+ *     this is passed as part of a rule collection to createValidator
+ */
+export function createRule(type, validate, payload = null) {
+  if (payload === null) {
+    return (change, prop) => (validate(change[prop]) ? null : { type, prop });
+  }
+  const getPayload =
+    payload instanceof Function ? result => payload(result) : () => payload;
+  return (change, prop) => {
+    if (validate(change[prop])) {
+      return null;
+    }
+    return { type, prop, payload: getPayload({ type, prop }) };
+  };
+}
+
+/**
+ * Helper for augmenting asynchronous validation rules with payloads.
+ * @param  {function} validate - callback that accepts a `change` object and
+ *     returns a list of { type, prop } objects for each element in the change
+ *     that failed has a failing rule.
+ * @param  {function|*} [payload=null] - data to augment the result with. If a
+ *     function is provided it will be called with the result object and the
+ *     response used as the result.payload
+ * @return {function} - function to call to apply the rule to a change; typically
+ *     this is passed as part of a rule collection to createValidator
+ */
+export function createAsyncRule(validate, payload = null) {
+  if (payload === null) {
+    return validate;
+  }
+  const getPayload =
+    payload instanceof Function ? result => payload(result) : () => payload;
+  return async change =>
+    (await validate(change)).map(result => ({
+      ...result,
+      payload: getPayload(result)
+    }));
+}
+
+/**
+ * Helper for transforming a list of validator results into a simpler object
+ * mapping field names to payloads.
+ * 
+ * @param  {[object]} results - List of validator result object containing { type, prop, payload }
+ * @return {object} - The results reduced to {[prop]: payload, ...}
+ */
+export function getPayload(results) {
+  return results.reduce(
+    (o, { prop, payload }) => ({ ...o, [prop]: payload }),
+    {}
+  );
+}
---
+++ test/createValidator.test.js
@@ -0,0 +1,252 @@
+import {
+  createValidator,
+  createRule,
+  createAsyncRule,
+  getPayload,
+  ValidatorError
+} from "../src";
+
+function createSimpleAsyncRule(type, validate) {
+  return async change =>
+    Object.keys(change).map(prop =>
+      validate(change[prop]) ? null : { type, prop }
+    );
+}
+
+function createMatchRule(type, data, field) {
+  return (change, prop) =>
+    data[field] === change[prop] ? null : { type, prop };
+}
+
+function createAsyncSubscriptionRule(type, validate) {
+  const subscribers = [];
+  const rule = jest.fn((change, prop, data) => {
+    return new Promise(resolve => {
+      subscribers.push(() =>
+        resolve(
+          Object.keys(change)
+            .filter(prop => !validate(change[prop]))
+            .map(prop => ({ type, prop }))
+        )
+      );
+    });
+  });
+  return [rule, () => subscribers.forEach(resolve => resolve())];
+}
+
+let validate, async1;
+beforeEach(() => {
+  const filled = createRule("filled", value => value.length > 0);
+  const rule1 = createRule("rule1", value => value !== "rule1");
+  const rule2 = createRule("rule2", value => value !== "rule2");
+  const rule3 = createRule("rule3", value => value !== "rule3");
+  async1 = jest.fn(
+    createSimpleAsyncRule("async1", value => value !== "async1")
+  );
+  const data = {
+    password: "abc123"
+  };
+  const matchPassword = createMatchRule("matchPassword", data, "password");
+  const rules = {
+    field1: [filled],
+    field2: [async1],
+    field3: [async1],
+    field4: [filled, matchPassword],
+    field5: [rule1, rule2, rule3, async1]
+  };
+  validate = createValidator(rules, data);
+});
+
+test("empty value fails 'filled' rule ", async () => {
+  const change = {
+    field1: ""
+  };
+  const results = await validate(change);
+  const expected = [
+    {
+      type: "filled",
+      prop: "field1"
+    }
+  ];
+  expect(results).toMatchObject(expected);
+});
+
+test("non-empty value passes 'filled' rule ", async () => {
+  const change = {
+    field1: "filled"
+  };
+  const results = await validate(change);
+  const expected = [];
+  expect(results).toMatchObject(expected);
+});
+
+test("async rule fails when condition is met", async () => {
+  const change = {
+    field2: "async1"
+  };
+  const results = await validate(change);
+  const expected = [
+    {
+      type: "async1",
+      prop: "field2"
+    }
+  ];
+  expect(results).toMatchObject(expected);
+});
+
+test("async rule can be used in multiple fields", async () => {
+  const change = {
+    field2: "filled",
+    field3: "async1"
+  };
+  const results = await validate(change);
+  const expected = [
+    {
+      type: "async1",
+      prop: "field3"
+    }
+  ];
+  expect(results).toMatchObject(expected);
+  expect(async1).toHaveBeenCalledTimes(1);
+});
+
+test("able to do a password check validation", async () => {
+  const results = await validate({ field4: "abc" });
+  const expected = [
+    {
+      type: "matchPassword",
+      prop: "field4"
+    }
+  ];
+  expect(results).toMatchObject(expected);
+  const goodResults = await validate({ field4: "abc123" });
+  expect(goodResults).toHaveLength(0);
+});
+
+test("validation returns first failing rule", async () => {
+  expect((await validate({ field5: "rule1" }))[0].type).toEqual("rule1");
+  expect((await validate({ field5: "rule2" }))[0].type).toEqual("rule2");
+  expect((await validate({ field5: "rule3" }))[0].type).toEqual("rule3");
+  expect((await validate({ field5: "async1" }))[0].type).toEqual("async1");
+});
+
+test("simulataneous validations", () => {
+  const [delayed, resolveDelayed] = createAsyncSubscriptionRule(
+    "delayed",
+    () => false
+  );
+  const rules = {
+    field1: [delayed],
+    field2: [delayed],
+    field3: [delayed]
+  };
+  const validate = createValidator(rules);
+  const change1 = { field1: "" };
+  const change2 = { field2: "" };
+  const change3 = { field3: "" };
+  validate(change1).catch(error => {
+    expect(error instanceof ValidatorError).toBe(true);
+    expect(error.validatorErrorPayload).toMatchObject([
+      { type: "delayed", prop: "field1" }
+    ]);
+  });
+  validate(change2).catch(error => {
+    expect(error instanceof ValidatorError).toBe(true);
+  });
+  validate(change3).then(results => {
+    const expected = [
+      { type: "delayed", prop: "field1" },
+      { type: "delayed", prop: "field2" },
+      { type: "delayed", prop: "field3" }
+    ];
+    expect(results).toMatchObject(expected);
+    expect(delayed).toHaveBeenCalledTimes(2);
+  });
+  expect(delayed).toHaveBeenCalledTimes(1);
+  resolveDelayed();
+});
+
+test("payload augmentation", async () => {
+  const rule1 = createRule("rule1", value => false, { text: "rule1" });
+  const rule2 = createRule(
+    "rule2",
+    value => false,
+    ({ type, prop }) => ({ text: `${type} ${prop}` })
+  );
+  const rules = {
+    field1: [rule1],
+    field2: [rule2]
+  };
+  const validate = createValidator(rules);
+  const change = {
+    field1: "",
+    field2: ""
+  };
+  const results = await validate(change);
+  const expected = [
+    {
+      type: "rule1",
+      prop: "field1",
+      payload: {
+        text: "rule1"
+      }
+    },
+    {
+      type: "rule2",
+      prop: "field2",
+      payload: {
+        text: "rule2 field2"
+      }
+    }
+  ];
+  expect(results).toMatchObject(expected);
+});
+
+test("async payload augmentation", async () => {
+  const rule1 = createRule("rule1", value => false);
+  const serverRules = {
+    field1: [rule1]
+  };
+  const serverValidate = createValidator(serverRules);
+  const server = createAsyncRule(
+    change => serverValidate(change),
+    ({ type, prop }) => `${type} ${prop}`
+  );
+  const rules = {
+    field1: [server]
+  };
+  const validate = createValidator(rules);
+  const change = {
+    field1: ""
+  };
+  const results = await validate(change);
+  const expected = [
+    {
+      type: "rule1",
+      prop: "field1",
+      payload: "rule1 field1"
+    }
+  ];
+  expect(results).toMatchObject(expected);
+});
+
+test("getPayload result transformation", () => {
+  const results = [
+    {
+      type: "rule1",
+      prop: "field1",
+      payload: "payload 1"
+    },
+    {
+      type: "rule2",
+      prop: "field2",
+      payload: "payload 2"
+    }
+  ];
+  const messages = getPayload(results);
+  const expected = {
+    field1: "payload 1",
+    field2: "payload 2"
+  };
+  expect(messages).toMatchObject(expected);
+});
